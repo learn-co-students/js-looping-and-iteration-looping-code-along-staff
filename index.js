c44f01049fe050a5a70f2413c5cdbdd73c783d84
hoffm386 <erinrhof@uw.edu>
2019-06-03 19:34:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(nameList, message) {
+  const cards = [];
+  for (let i=0; i<nameList.length; i++) {
+    cards.push(`Thank you, ${nameList[i]}, for the wonderful ${message} gift!`);
+  }
+  return cards;
+}
+
+function countdown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
